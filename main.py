8d9c781dcc2e8de4590296a49fd99f17bfe92212
João Batista <batistajoao54.ba@gmail.com>
2022-08-10 20:07:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #importando as bibliotecas que iremos usar para essa versão
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+import plotly.figure_factory as ff
 
 #---------------------------------------------------------------------------
 
@@ -9,57 +11,80 @@ st.set_page_config(layout='wide')
 
 #--------------------------------------------------------------------------
 
-#criando componentes do titulo e do carregamento de dados
-col_titulo, col_dados = st.columns([2,1])
+#criando componentes do titulo e dos graficos
+col_titulo, col_graficos = st.columns([1,1])
+
+df_clientes = pd.read_csv('clientes.csv')
+df_cidades = pd.read_csv('cidade.csv')
+df_estado = df_clientes.groupby('ESTADO').count().reset_index()
+
 
 with col_titulo:
-  st.title("Controle de distribuição de sacolas")
+  st.markdown("### CLIENTES/CIDADES")
 
-with col_dados:
-  dados = st.file_uploader("Add dados")
+  #separando os clientes
+
+  lista_clientes = list(df_clientes['LOJA'].unique())
+  lista_clientes.insert(0," ")
+
+  pesquisa = st.selectbox("NOME DO CLIENTE",lista_clientes)
+
+  df_pesquisa = df_clientes[df_clientes['LOJA'] == pesquisa]
+
+  if pesquisa == ' ':
+    st.markdown("##### Faça uma pesquisa")
+
+  else:
+    fig2 = ff.create_table(df_pesquisa,height_constant=10)
+    st.plotly_chart(fig2,use_container_width=True)
+
 #-----------------------------------------------------------------------------
 
-#configurando os modos de pesquisas
-pesquisa = st.radio('MODO DA PESQUISA', ('','NOME','MARCA','OS'))
+#preenchendo os graficos
+with col_graficos:
+  st.markdown('### CLIENTES POR ESTADO')
 
-#-------------------------------------------------------------------------------
+  fig = px.bar(df_estado, x='ESTADO', y='LOJA', color='ESTADO', text_auto=True, )
+  fig.update_xaxes(title='ESTADOS COM CLIENTES')
+  fig.update_yaxes(title='QUANTIDADE DE CLIENTES')
+  st.plotly_chart(fig,use_container_width=True)
 
-#configurando as pesquisas por Nome
-if pesquisa == "NOME":
-  col_nome,col_marca,col_os = st.columns(3)
-  df = pd.read_csv(dados)
+#criando um grafico de mapa
+butao = st.checkbox('MOSTRAR GRAFICO DE BARRA')
 
-  lista_nome = list(df['pessoa'].unique())
-  lista_nome.insert(0,'')
-  with col_nome:
-    nome = st.selectbox("Nome",lista_nome)
-    df_nome = df[df['pessoa'] == nome]
+if butao == False:
+  st.markdown("##### CLIENTES PELO BRASIL")
 
+  fig_map = px.scatter_mapbox(
+    df_cidades,
+    lat='LAT',
+    lon='LONG',
+    hover_name='CIDADE',
+    #hover_date=["ESTADO","CIDADE"],
+    size='LOJA',
+    color_continuous_scale=px.colors.cyclical.IceFire,
+    size_max=45,
+    zoom=5.45,
 
-  lista_marca = list(df_nome['marca'].unique())
-  #lista_marca.insert(0, '')
-  with col_marca:
-    marca = st.selectbox('MARCA',lista_marca)
-    df_marca = df_nome[df_nome['marca'] == marca]
+  )
 
+  fig_map.update_layout(mapbox_style='open-street-map')
 
-  lista_os = list(df_marca['os'].unique())
-  #lista_os.insert(0, '')
-  with col_os:
-    os = st.selectbox('OS',lista_os)
-    df_os = df_marca[df_marca['os'] == os]
+  fig_map.update_layout(
+    height=400,
+    margin={
+      'r': 0,
+      't': 0,
+      'l': 0,
+      'b': 0
+    }
+  )
 
-  st.table(df_os)
+  st.plotly_chart(fig_map,use_container_width=True)
 
+else:
+  fig_barra = px.bar(df_cidades, x='CIDADE', y='LOJA', color='ESTADO', text_auto=True, )
+  fig_barra.update_xaxes(title='CIDADES')
+  fig_barra.update_yaxes(title='QUANTIDADE DE CLIENTES')
 
-
-#---------------------------------------------------------------------------------
-
-
-#df = pd.read_csv(dados)
-#st.table(df)
-
-
-
-
-
+  st.plotly_chart(fig_barra,use_container_width=True)
